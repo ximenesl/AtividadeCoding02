1c860eb4723a330d6cc44ce19ae293202e339c2f
Lucas Ximenes <lcsximenes13@gmail.com>
2026-03-30 13:45:38 -0300

    Fix: SW

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,17 +1,28 @@
 const CACHE_NAME = 'pussycat-v1';
 const ASSETS = [
+  '/',
   'index.html',
   'style.css',
   'script.js',
-  'manifest.json'
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
 ];
 
+// Instalação: Armazena o "esqueleto" do app no cache
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Cacheando arquivos do app shell');
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
+
+self.addEventListener('activate', (e) => {
+  console.log('Service Worker ativo');
+});
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
